36b8d2b8701f672e028967f9c65dd316d1c0a88a
19810322 <l98lo322@gmail.com>
2019-08-14 21:57:24 +0900

    Fixed some bugs.

--- chkmdstat.sh
+++ chkmdstat.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
 #--- Modify here ---
+#FLAGFILE is prevent for send message repeatedly.
 FLAGFILE="/tmp/mdflag"
+#SLACKURI is uri for webhook.
+#You can get uri via your slack app page.
+SLACKURI=''
+#LINETOKEN is token for LINE notify.
+#You can get token via your LINE notify account page.
 LINETOKEN="INPUT HERE"
+#TOKAROTOKEN is token for webhook integration.
+#You can get token via your TOCARO's integration page.
 TOCAROTOKEN="INPUT HERE"
 MESSAGE="Degraded disk array detected. Please check Machine's status!"
 HOSTNAME=`hostname`
 #--- Modify here ---
 
-#delete flagfile when bootig machine
+#chkmdstat deletes flagfile when execute with initialize option.
 #Please command line option 'init' when this script add rc.local
 if [ $# -eq 1 ]; then
 	if [ $1 = "init" ]; then
@@ -25,26 +33,36 @@ if [ -e $FLAGFILE ]; then
 fi
 
 #for debug
-#MDSTAT=`cat mdstat-d.txt | grep -c _`
-MDSTAT=`cat /proc/mdstat | grep -c _`
+MDSTAT=`cat ~/mdstat-d.txt | grep -c _`
+#MDSTAT=`cat /proc/mdstat | grep -c _`
 
 if [ $MDSTAT = "0" ]; then
 	exit 0
 fi
 
-if [ -e $FLAGFILE ]; then
-	exit 0
-fi
-
 touch $FLAGFILE
 
+#Slack
+if [ $SLACKURI != "INPUT HERE" ]; then
+	curl -f -X POST --data-urlencode "payload={ \"username\": \”chkmdstat\”, \"text\": \”$HOSTNAME: $MESSAGE\” "}" $SLACKURI
+	if [ $? -gt 0 ]; then
+		rm $FLAGFILE
+	fi
+fi
+
 #LINE Notify
-curl -X POST -H "Authorization: Bearer $LINETOKEN" -F "message=$HOSTNAME: $MESSAGE" https://notify-api.line.me/api/notify
-if [ $? -gt 0 ]; then
-	rm $FLAGFILE
+if [ $LINETOKEN != "INPUT HERE" ]; then
+	curl -f -X POST -H "Authorization: Bearer $LINETOKEN" -F "message=$HOSTNAME: $MESSAGE" https://notify-api.line.me/api/notify
+		if [ $? -gt 0 ]; then
+		rm $FLAGFILE
+	fi
 fi
+
 #Tocaro
-curl -X POST -H "Content-type: application/json" --data "{\"text\": \"$HOSTNAME: $MESSAGE\" }" https://hooks.tocaro.im/integrations/inbound_webhook/$TOCAROTOKEN
-if [ $? -gt 0 ]; then
-	rm $FLAGFILE
+if [ $TOKAROTOKEN != "INPUT HERE" ]; then
+	curl -f -X POST -H "Content-type: application/json" --data "{\"text\": \"$HOSTNAME: $MESSAGE\" "}" https://hooks.tocaro.im/integrations/inbound_webhook/$TOCAROTOKEN
+	if [ $? -gt 0 ]; then
+		rm $FLAGFILE
+	fi
 fi
+
